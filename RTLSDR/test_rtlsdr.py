195de9056ceb587c246affe5b47adb1219e57558
Ridha Chowdhury <chowdhuryridha@gmail.com>
2023-04-03 00:36:15 -0400

    rtlsdr power reading

--- RTLSDR/test_rtlsdr.py
+++ RTLSDR/test_rtlsdr.py
@@ -1,31 +1,29 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jul 26 21:55:36 2017
+from pylab import *
+from rtlsdr import *
 
-@author: jaguirre
-"""
+sdr = RtlSdr()
 
-import matplotlib.pyplot as plt
-import rtlsdr 
-
-sdr = rtlsdr.RtlSdr()
-
-#%%
 # configure device
-sdr.sample_rate = 3.0e6
-sdr.center_freq = 260.5e6
-sdr.gain = 'auto'#4
+sdr.sample_rate = 2.4e6
+sdr.center_freq = 1.42e9
+sdr.gain = 'auto'
+
+# perform IQ correction
+sdr.set_freq_correction(60)
+sdr.set_iq_balance_mode(True)
 
 samples = sdr.read_samples(256*1024)
-
-plt.figure()
-plt.clf()
-# use matplotlib to estimate and plot the PSD
-plt.psd(samples, NFFT=1024, Fs=sdr.sample_rate/1e6, Fc=sdr.center_freq/1e6)
-plt.xlabel('Frequency (MHz)')
-plt.ylabel('Relative power (dB)')
-
 sdr.close()
-plt.show()
 
+# calculate power from IQ samples
+power = 10*np.log10(np.mean(np.abs(samples)**2))
+
+# use matplotlib to estimate and plot the PSD
+psd(samples, NFFT=1024, Fs=sdr.sample_rate/1e6, Fc=sdr.center_freq/1e6) # expects frequency in MHz
+xlabel('Frequency (MHz)')
+ylabel('Relative power (dB)')
+
+# display the calculated power
+print(f"Power: {power:.2f} dBFS")
+
+show()
