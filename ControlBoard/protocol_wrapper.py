7f278dc4b31851ece6f0d23ffb37d2b034e70541
Mark Peschel <mpeschel10@gmail.com>
2023-04-20 12:29:52 -0400

    Move protocol constants to ControlBoard/protocol.py

--- ControlBoard/protocol_wrapper.py
+++ ControlBoard/protocol_wrapper.py
@@ -1,28 +1,6 @@
 from ControlBoard.port_wrapper import PortWrapper, FakePortWrapper
 
-# Commands to PCB:
-EOT = chr(4).encode('ASCII')
-
-CALIBRATE_ALT =   'cl'.encode('ASCII')
-CALIBRATE_AZ =    'cz'.encode('ASCII')
-CALIBRATE_TWIST = 'ct'.encode('ASCII')
-
-MOVE_ALT_PLUS =   'ml'.encode('ASCII')
-MOVE_ALT_MINUS =  'wl'.encode('ASCII')
-MOVE_AZ_PLUS =    'mz'.encode('ASCII')
-MOVE_AZ_MINUS =   'wz'.encode('ASCII')
-
-GOTO_ALT_AZ =     'g '.encode('ASCII')
-
-STOP_ALT =        'xl'.encode('ASCII')
-STOP_AZ =         'xz'.encode('ASCII')
-STOP_ALL =        'xa'.encode('ASCII')
-GET_STATUS =      'r '.encode('ASCII')
-
-FLAGS = [
-    (1 << 15, "Bad command; start over"),
-    (1 << 14, "Moving"),                    # Stationary by default
-]
+from ControlBoard.protocol import *
 
 def encode_degrees(num):
     if num < 0:
